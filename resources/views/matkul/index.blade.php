4af472989a3dc30d0e965645ed3ed26da873638f
Devinashifra <devina.shifra.ft20@mail.umy.ac.id>
2023-07-13 23:33:07 +0700

    menambahkan halaman

--- resources/views/matkul/index.blade.php
+++ resources/views/matkul/index.blade.php
@@ -28,7 +28,7 @@
                         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                             <tr>
                                 <th scope="col" class="px-6 py-3">
-                                    Nama Mata Kuliah
+                                   Nama Mata Kuliah
                                 </th>
                                 <th scope="col" class="px-6 py-3">
                                     Action
