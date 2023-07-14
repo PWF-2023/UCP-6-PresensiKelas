a49976decbcda5a9215e890476cc25238352426e
fatikahamzah <fatikahmzptr@gmail.com>
2023-07-14 13:23:16 +0700

    menambahkan controller

--- app/Http/Controllers/MatkulController.php
+++ app/Http/Controllers/MatkulController.php
@@ -3,8 +3,66 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Matkul;
 
 class MatkulController extends Controller
 {
-    //
+    public function index()
+    {
+        $matkuls = Matkul::all();
+        return view('matkul.index', compact('matkuls'));
+    }
+
+    public function create()
+    {
+        return view('matkul.create');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'nama' => 'required',
+            'kode' => 'required|unique:matkuls',
+            'sks' => 'required|numeric|min:1'
+        ]);
+
+        Matkul::create([
+            'nama' => $request->nama,
+            'kode' => $request->kode,
+            'sks' => $request->sks
+        ]);
+
+        return redirect()->route('matkul.index')->with('success', 'Mata kuliah berhasil ditambahkan');
+    }
+
+    public function edit($id)
+    {
+        $matkul = Matkul::find($id);
+        return view('matkul.edit', compact('matkul'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'nama' => 'required',
+            'kode' => 'required|unique:matkuls,kode,' . $id,
+            'sks' => 'required|numeric|min:1'
+        ]);
+
+        $matkul = Matkul::find($id);
+        $matkul->nama = $request->nama;
+        $matkul->kode = $request->kode;
+        $matkul->sks = $request->sks;
+        $matkul->save();
+
+        return redirect()->route('matkul.index')->with('success', 'Mata kuliah berhasil diperbarui');
+    }
+
+    public function destroy($id)
+    {
+        $matkul = Matkul::find($id);
+        $matkul->delete();
+
+        return redirect()->route('matkul.index')->with('success', 'Mata kuliah berhasil dihapus');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PresensiController;
+use App\Http\Controllers\MatkulController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -23,9 +25,29 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::get('/prsofile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+Route::middleware('auth')->group(function () {
+    Route::get('/presensi', [PresensiController::class, 'index'])->name('presensi.index');
+    Route::get('/presensi/create', [PresensiController::class, 'create'])->name('presensi.create');
+    Route::post('/presensi', [PresensiController::class, 'store'])->name('presensi.store');
+    Route::get('/presensi/{id}/edit', [PresensiController::class, 'edit'])->name('presensi.edit');
+    Route::patch('/presensi/{id}', [PresensiController::class, 'update'])->name('presensi.update');
+    Route::delete('/presensi/{id}', [PresensiController::class, 'destroy'])->name('presensi.destroy');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/matkul', [MatkulController::class, 'index'])->name('matkul.index');
+    Route::get('/matkul/create', [MatkulController::class, 'create'])->name('matkul.create');
+    Route::post('/matkul', [MatkulController::class, 'store'])->name('matkul.store');
+    Route::get('/matkul/{id}/edit', [MatkulController::class, 'edit'])->name('matkul.edit');
+    Route::patch('/matkul/{id}', [MatkulController::class, 'update'])->name('matkul.update');
+    Route::delete('/matkul/{id}', [MatkulController::class, 'destroy'])->name('matkul.destroy');
+});
+
 
 require __DIR__.'/auth.php';
+
+
